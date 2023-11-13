3055855ee977704864300b35298fd768eab3e0cc
LEONARDO PEREIRA <30580018+LeonardoPereirajr@users.noreply.github.com>
2023-11-13 18:59:10 -0300

    Add new API endpoints for plans and customer plans

--- CloudParkApp/urls.py
+++ CloudParkApp/urls.py
@@ -8,5 +8,11 @@ urlpatterns = [
     path('api/v1/vehicle/', views.vehicle_api),
     path('api/v1/vehicle/<int:id>/', views.vehicle_api),
 
+    path('api/v1/plan/', views.plan_api),
+    path('api/v1/plan/<int:id>/', views.plan_api),
+
+    path('api/v1/customerplan/', views.customer_plan_api),
+    path('api/v1/customerplan/<int:id>/', views.customer_plan_api),
+
     
 ]
--- CloudParkApp/views.py
+++ CloudParkApp/views.py
@@ -88,3 +88,69 @@ def vehicle_api(request, id=0):
         vehicle = Vehicle.objects.get(id=id)
         vehicle.delete()
         return JsonResponse("Deleted Successfully!!", safe=False)
+
+@csrf_exempt
+def plan_api(request, id=0):
+    if request.method == 'GET':
+        plans = Plan.objects.all()
+        plan_serializer = PlanSerializer(plans, many=True)
+        return JsonResponse(plan_serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        plan_data = JSONParser().parse(request)
+        plan_serializer = PlanSerializer(data=plan_data)
+        if plan_serializer.is_valid():
+            plan_serializer.save()
+            return JsonResponse("Added Successfully!!", safe=False)
+        return JsonResponse("Failed to Add.", safe=False)
+
+    elif request.method == 'PUT':
+        plan_data = JSONParser().parse(request)
+        plan = Plan.objects.get(id=plan_data['id'])
+        plan_serializer = PlanSerializer(plan, data=plan_data)
+        if plan_serializer.is_valid():
+            plan_serializer.save()
+            return JsonResponse("Updated Successfully!!", safe=False)
+        return JsonResponse("Failed to Update.", safe=False)
+
+    elif request.method == 'DELETE':
+        plan = Plan.objects.get(id=id)
+        plan.delete()
+        return JsonResponse("Deleted Successfully!!", safe=False)
+
+@csrf_exempt
+def customer_plan_api(request, id=0):
+    if request.method == 'GET':
+        customer_plans = CustomerPlan.objects.all()
+        customer_plan_serializer = CustomerPlanSerializer(customer_plans, many=True)
+        return JsonResponse(customer_plan_serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        customer_plan_data = JSONParser().parse(request)
+        existing_customer_plan = CustomerPlan.objects.filter(
+            customer_id=customer_plan_data['customer_id'],
+            plan_id=customer_plan_data['plan_id']
+        ).first()
+
+        if existing_customer_plan:
+            return JsonResponse("CustomerPlan already exists.", safe=False)
+
+        customer_plan_serializer = CustomerPlanSerializer(data=customer_plan_data)
+        if customer_plan_serializer.is_valid():
+            customer_plan_serializer.save()
+            return JsonResponse("Added Successfully!!", safe=False)
+        return JsonResponse("Failed to Add.", safe=False)
+
+    elif request.method == 'PUT':
+        customer_plan_data = JSONParser().parse(request)
+        customer_plan = CustomerPlan.objects.get(id=customer_plan_data['id'])
+        customer_plan_serializer = CustomerPlanSerializer(customer_plan, data=customer_plan_data)
+        if customer_plan_serializer.is_valid():
+            customer_plan_serializer.save()
+            return JsonResponse("Updated Successfully!!", safe=False)
+        return JsonResponse("Failed to Update.", safe=False)
+
+    elif request.method == 'DELETE':
+        customer_plan = CustomerPlan.objects.get(id=id)
+        customer_plan.delete()
+        return JsonResponse("Deleted Successfully!!", safe=False)
