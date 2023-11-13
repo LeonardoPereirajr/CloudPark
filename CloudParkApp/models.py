204e50bfa2d36022f41e50dfa1b433d771a02e65
LEONARDO PEREIRA <30580018+LeonardoPereirajr@users.noreply.github.com>
2023-11-13 19:56:40 -0300

    Added contract rules to Contract model and implemented CRUD API for contracts

--- CloudParkApp/models.py
+++ CloudParkApp/models.py
@@ -27,6 +27,7 @@ class Contract(models.Model):
     id = models.AutoField(primary_key=True)
     description = models.CharField(max_length=50)
     max_value = models.FloatField(null=True)
+    rules = models.ManyToManyField('ContractRule', blank=True)
 
 class ContractRule(models.Model):
     id = models.AutoField(primary_key=True)
--- CloudParkApp/urls.py
+++ CloudParkApp/urls.py
@@ -5,14 +5,18 @@ urlpatterns = [
     path('api/v1/customer/', views.customer_api),
     path('api/v1/customer/<int:id>/', views.customer_api),
     
+    path('api/v1/plan/', views.plan_api),
+    path('api/v1/plan/<int:id>/', views.plan_api),
+   
     path('api/v1/vehicle/', views.vehicle_api),
     path('api/v1/vehicle/<int:id>/', views.vehicle_api),
 
-    path('api/v1/plan/', views.plan_api),
-    path('api/v1/plan/<int:id>/', views.plan_api),
-
+    path('api/v1/contract/', views.contract_api),
+    path('api/v1/contract/<int:id>/', views.contract_api),
+    
     path('api/v1/customerplan/', views.customer_plan_api),
     path('api/v1/customerplan/<int:id>/', views.customer_plan_api),
+
 
     
 ]
