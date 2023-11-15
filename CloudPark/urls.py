4207ab18a60519c4ed149717ba0b2f2330a5b1f7
LEONARDO PEREIRA <30580018+LeonardoPereirajr@users.noreply.github.com>
2023-11-15 07:19:47 -0300

    Refactor - padronização de pastas

--- CloudPark/urls.py
+++ CloudPark/urls.py
@@ -3,5 +3,10 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('CloudParkApp.urls'))
+    path('', include('CloudParkApp.vehicle.urls')),
+    path('', include('CloudParkApp.customer.urls')),
+    path('', include('CloudParkApp.customer_plan.urls')),
+    path('', include('CloudParkApp.contract.urls')),
+    path('', include('CloudParkApp.plan.urls')),
+    path('', include('CloudParkApp.parkmovement.urls')),
 ]
